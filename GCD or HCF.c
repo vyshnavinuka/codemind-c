4c87a6e07bf5de2f35ccae175fb9de36f57c89e8
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-01-19 21:01:44 +0530

    #include<stdio.h> main() {     int a,b,i,t;     scanf("%d%d",&a,&b);     if(a<b)     {         t=b;         b=a;         a=t;     }     for(i=b;i>=1;i--)     {         if(a%i==0&&b%i==0)         {         printf("%d",i);         break;         }     } }

---
+++ GCD or HCF.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+    int a,b,i,t;
+    scanf("%d%d",&a,&b);
+    if(a<b)
+    {
+        t=b;
+        b=a;
+        a=t;
+    }
+    for(i=b;i>=1;i--)
+    {
+        if(a%i==0&&b%i==0)
+        {
+        printf("%d",i);
+        break;
+        }
+    }
+}
