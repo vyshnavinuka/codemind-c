0953b883ad03dff3369db7a4bcb2497444f11b82
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-05-26 22:14:18 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[200];     int count=0,i;     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' '&& s[i+1]!=' ')         count++;     }     printf("%d ",count+1); }

---
+++ Count_no._of_words.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[200];
+    int count=0,i;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' '&& s[i+1]!=' ')
+        count++;
+    }
+    printf("%d
+",count+1);
+}
