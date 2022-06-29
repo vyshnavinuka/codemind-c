460d12e496f1fc5e69669d3ac1164f7608ea6340
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-29 20:29:52 +0530

    #include<stdio.h> int main() {     int n,a,i;     int s=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         if(i+2<n)         {             if(ar[i]%2==1 && ar[i+2]%2==1)             {                 if(ar[i+1]%2==1)                 {                     s++;                 }             }         }     }     printf("%d",s); }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i;
+    int s=0;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i+2<n)
+        {
+            if(ar[i]%2==1 && ar[i+2]%2==1)
+            {
+                if(ar[i+1]%2==1)
+                {
+                    s++;
+                }
+            }
+        }
+    }
+    printf("%d",s);
+}
