39ed1c52a31dec0727c68f5eba17d7683d99c477
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 08:34:27 +0530

    #include<stdio.h> int main() {     int n,i,a,b;     int s=0,p=0;     scanf("%d",&n);     int ar[n],c[n],d=0;     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(ar[i]<a || ar[i]>b)         {             c[d]=ar[i];             //printf("%d ",c[d]);             d++;         }     }     int max=0;     for(i=0;i<d;i++)     {         if(c[i]>max)         {             max=c[i];         }     }     if(d==0)     {        printf("-1");     }     else     {         printf("%d",max);     } }

---
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b;
+    int s=0,p=0;
+    scanf("%d",&n);
+    int ar[n],c[n],d=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]<a || ar[i]>b)
+        {
+            c[d]=ar[i];
+            //printf("%d ",c[d]);
+            d++;
+        }
+    }
+    int max=0;
+    for(i=0;i<d;i++)
+    {
+        if(c[i]>max)
+        {
+            max=c[i];
+        }
+    }
+    if(d==0)
+    {
+       printf("-1"); 
+    }
+    else
+    {
+        printf("%d",max);
+    }
+}
