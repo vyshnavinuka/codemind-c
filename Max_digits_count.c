691eaf0777e5e938e8fcfdd013e561c946e17a1b
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 15:01:06 +0530

    #include<stdio.h> int main() {     int i,j,n,c=0,d=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         //printf("%d ",a[i]);     }     int min=a[0];     for(i=0;i<n;i++)     {         if(min<a[i])         {             min=a[i];         }     }     //printf("
         while(min)
                {
                    min=min/10;
                    d++;
                }
        int k=0;
        for(i=0;i<n;i++)
        {
            c=0;
            j=a[i];
            if(j==0)
            {
                c=1;
            }
            else
            {
                while(j)
                {
                    j=j/10;
                    c++;
                }
            }
            if(d==c)
            {
                k++;
            }
        }
        printf("%d",k);
    }

---
+++ Max_digits_count.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,c=0,d=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        //printf("%d ",a[i]);
+    }
+    int min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min<a[i])
+        {
+            min=a[i];
+        }
+    }
+    //printf("
+
+     while(min)
+            {
+                min=min/10;
+                d++;
+            }
+    int k=0;
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        j=a[i];
+        if(j==0)
+        {
+            c=1;
+        }
+        else
+        {
+            while(j)
+            {
+                j=j/10;
+                c++;
+            }
+        }
+        if(d==c)
+        {
+            k++;
+        }
+    }
+    printf("%d",k);
+}
