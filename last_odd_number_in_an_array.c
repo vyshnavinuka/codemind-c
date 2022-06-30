792251a53881326a7256a2036858166cd9705af7
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 08:12:59 +0530

    #include<stdio.h> int main() {     int n,i,a;     int s=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=n-1;i>=0;i--)     {         if(ar[i]%2==1)         {             s=ar[i];             break;         }     }     printf("%d",s); }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a;
+    int s=0;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(ar[i]%2==1)
+        {
+            s=ar[i];
+            break;
+        }
+    }
+    printf("%d",s);
+}
