ac7caff814ee5d9e2dae4cb1b41ead20b7307a33
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 08:21:35 +0530

    #include<stdio.h> int main() {     int n,i,a,b;     int s=0,p=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(ar[i]>=a&&ar[i]<=b)             s=s+ar[i];         p=p+ar[i];     }     printf("%d",p-s); }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b;
+    int s=0,p=0;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]>=a&&ar[i]<=b)
+            s=s+ar[i];
+        p=p+ar[i];
+    }
+    printf("%d",p-s);
+}
