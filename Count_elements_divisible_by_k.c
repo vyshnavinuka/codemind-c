c5661ab25909cefdd33dbf37fb1267639e8c49f0
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-10-06 21:51:28 +0530

    #include<stdio.h> int main() {     int n,i,c=0,arr[100],k;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k==0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,arr[100],k;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
