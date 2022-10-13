e11581f4bcbfc97a7c676468b4b043497e853185
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-10-13 19:50:45 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,p,r=0,c,d;     scanf("%d",&n);     while(n!=0)     {         n++;         p=n;         while(p!=0)         {             r=r*10;             r=r+p%10;             p=p/10;         }         if(r==n)         {             d=sqrt(n);             for(c=2;c<=d;c++)             {                 if(n%c==0)                 {                     break;                 }             }             if(c==d+1)             {                 break;             }         }         r=0;     }     printf("%d",n); }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,p,r=0,c,d;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        n++;
+        p=n;
+        while(p!=0)
+        {
+            r=r*10;
+            r=r+p%10;
+            p=p/10;
+        }
+        if(r==n)
+        {
+            d=sqrt(n);
+            for(c=2;c<=d;c++)
+            {
+                if(n%c==0)
+                {
+                    break;
+                }
+            }
+            if(c==d+1)
+            {
+                break;
+            }
+        }
+        r=0;
+    }
+    printf("%d",n);
+}
